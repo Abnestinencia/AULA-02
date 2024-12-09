3a12b0d87fa75748e0c9f89cffe2a73789c5f9e5
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-09 19:06:28 -0300

    final

--- index.js
+++ index.js
@@ -123,7 +123,9 @@ function cadastrarAluno(req, resp){
     resp.write(`
         <html>
           <head>
-            <title>
+            <title></title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="utf-8">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
           </head>
           <body>
@@ -154,6 +156,7 @@ function cadastrarAluno(req, resp){
     resp.write(`</tbody>
           </table>
           <a class="btn btn-primary" href="/cadastrarAluno">Continuar Cadastrando</a>
+          <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
           </body>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </html>`);
